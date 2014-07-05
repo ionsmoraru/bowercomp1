8704e0d17ad31d1b9e70806aba2f2b419910f89a
Ion Moraru <Ion.Moraru@td.com>
2014-07-05 14:03:45 -0400

    test files

--- app/scripts/bwtstcomp1.js
+++ app/scripts/bwtstcomp1.js
@@ -2,16 +2,18 @@
  * Sample directive implementation.
  * The module is declared in bwtstcomp1.js
  */
+angular.module('bwtstcomp1',[]);
+
 angular.module('bwtstcomp1').directive('compTest1', function () {
 
 	'use strict';
 
 	return {
-
-		template: '<div>Test Comp1</div>',
-
-		replace: true
-
+    restrict: 'AE',
+    scope: {
+      msg: '@tdMsg'
+    },
+		template: '<div>{{msg}}</div>'
 	};
 
 });

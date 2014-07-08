06f6cd23ea1d39ddf5e3d43b15fccbc6c1f14a7f
Ion Moraru <Ion.Moraru@td.com>
2014-07-08 13:01:06 -0400

    distribution

---
+++ dist/scripts/bwtstcomp1.44a49b8c.js
@@ -0,0 +1,1 @@
+angular.module("bwtstcomp1",[]),angular.module("bwtstcomp1").directive("compTest1",function(){"use strict";return{restrict:"AE",scope:{msg:"@tdMsg"},template:"<div>{{msg}}</div>"}});
--- test/spec/main.js
+++ test/spec/main.js
@@ -3,16 +3,28 @@
 describe('MyApp', function () {
 
   // load the module
-  beforeEach(module('bowercomp1'));
+  beforeEach(module('bwtstcomp1'));
 
-  var scope;
+  var scope,
+    compile;
 
   // Initialize the controller and a mock scope
-  beforeEach(inject(function ($rootScope) {
-    scope = $rootScope.$new();
+  beforeEach(inject(function (_$rootScope_, _$compile_) {
+    scope = _$rootScope_;
+    compile = _$compile_;
   }));
 
-  it('should be true', function () {
-    expect(3).toBe(3);
+  it('should load the template', function () {
+    var linkFn, directive;
+
+    linkFn = compile('<div data-comp-test1 td-msg="{{msg}}"></div>');
+
+    directive = linkFn(scope);
+
+    scope.msg = 'My test message';
+
+    scope.$digest();
+
+    expect(directive[0].innerText).toContain('test message');
   });
 });
